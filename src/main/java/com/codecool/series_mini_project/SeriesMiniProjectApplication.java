a0a0a71644a5588986e396f5c4d276a202d22c09
Kisleto <zsviktor45@gmail.com>
2019-02-20 19:57:31 +0100

    Initial commit

---
+++ src/main/java/com/codecool/series_mini_project/SeriesMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.series_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesMiniProjectApplication.class, args);
+    }
+
+}
